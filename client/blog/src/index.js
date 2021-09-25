f3e88e822cd586d3e6ffbd54f5d71ecbdf95cefb
Krae Wind <kraewind@gmail.com>
2021-09-25 15:51:47 -0400

    added store with dev tools for redux

--- client/blog/src/index.js
+++ client/blog/src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension'
+
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />

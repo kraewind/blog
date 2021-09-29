6231509a96c4e5ae5c5d3b54a2abbdb1436a0813
Krae Wind <kraewind@gmail.com>
2021-09-29 10:17:23 -0400

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2021_09_19_204117) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

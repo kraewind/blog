e65b34825d41a12a5302c8011d6048829f914701
Krae Wind <kraewind@gmail.com>
2021-09-29 10:25:39 -0400

    able to open development server

--- client/blog/src/index.js
+++ client/blog/src/index.js
@@ -8,9 +8,9 @@ import { createStore, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension'
 
 import thunk from 'redux-thunk';
-import rootReducer from './reducers/index';
+// import rootReducer from './reducers/index';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+// const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 
 ReactDOM.render(
